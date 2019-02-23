dd39802c3d3279d390747cf43f031cfc1c519a68
agusamani <47245109+agusamani@users.noreply.github.com>
2019-02-23 17:24:25 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { random } from 'lodash';
+import 'typeface-roboto';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import QuoteMachine from './components/QuoteMachine';
+
+const styles = {
+  container: {
+    display:'flex',
+    height: '100vh',
+    alignItems: 'center',
+  }
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        quotes: [],
+        selectedQuoteIndex: null,
+      }
+      this.generateNewQuoteIndex = this.generateNewQuoteIndex.bind(this);
+      this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+    .then(data => data.json())
+    .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
+  }
+
+  get selectedQuote() {
+    if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+      return undefined;
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+  
+/*
+Returns an integer between representing an index in state.quotes
+if  state.quotes is empty, return undefined
+*/ 
+ generateNewQuoteIndex() {
+    if( !this.state.quotes.length ) {
+      return undefined;
+    }
+      return random(0, this.state.quotes.length - 1);
+  }
+
+  assignNewQuoteIndex() {
+    this.setState({selectedQuoteIndex: this.generateNewQuoteIndex()});
+
+  }
+
+  render() {
+    return (
+      <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
+        <Grid xs={11} lg={8} item>
+          {
+             this.selectedQuote ? 
+             <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} /> 
+             : null 
+          }
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
---
+++ src/components/QuoteMachine.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+
+
+const QuoteMachine = (props) => (
+  <Card>
+    <CardContent>
+        <Typography id="text">
+          {props.selectedQuote.quote} - <span id="author"> {props.selectedQuote.author} </span>
+        </Typography>     
+    </CardContent>
+    <CardActions>
+      <Button id="new-quote" size="small" onClick={props.assignNewQuoteIndex}>Next Quote</Button>
+      <IconButton
+         id="tweet-quote"
+         target="_blank"
+         href={encodeURI(`https://twitter.com/intent/tweet?text=${props.selectedQuote.quote}&hastag=agustinamani`)}
+      >
+        <FontAwesomeIcon icon={faTwitter} size='md'></FontAwesomeIcon>
+      </IconButton>
+    </CardActions>
+  </Card>
+);
+
+export default QuoteMachine;
